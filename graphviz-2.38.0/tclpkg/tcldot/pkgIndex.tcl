667da634a343d64c3537f1c246c850d20f22b3b5
MSCho <hephaex@gmail.com>
2014-08-26 16:07:12 +0900

    graphviz-2.38.0

---
+++ graphviz-2.38.0/tclpkg/tcldot/pkgIndex.tcl
@@ -0,0 +1,2 @@
+package ifneeded Tcldot 2.38.0 "
+	load [file join $dir libtcldot.0.dylib] Tcldot"
---
+++ graphviz-2.38.0/tclpkg/tclpathplan/pkgIndex.tcl
@@ -0,0 +1,2 @@
+package ifneeded Tclpathplan 2.38.0 "
+	load [file join $dir libtclplan.0.dylib] Tclpathplan"
