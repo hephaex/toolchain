aa030da7990913e8acdc074ba9d77574b15b194a
Mario Cho <hephaex@gmail.com>
2017-09-06 21:40:26 +0900

    For emacs install, prepare tools autoconf, automake, libtool, texinfo

--- autoconf_install.sh
+++ autoconf_install.sh
@@ -7,11 +7,13 @@ BUILD_DIR=obj
 
 # Get the latest version of automake and libtool because they have no latest symbol link.
 AUTOCONF_VERSION=$(curl -fL http://ftpmirror.gnu.org/autoconf | grep -E "autoconf.*tar.gz" | grep -vE "sig|asc" | awk -F \" '{print $4}' | sed 's/autoconf-//g' | sed 's/.tar.gz//g' | sort | tail -n 1)
-echo && echo "check latest version of aotomake-${AUTOCONF_VERSION}"
+echo && echo "check latest version of autoconf-${AUTOCONF_VERSION}"
 AUTOMAKE_VERSION=$(curl -fL http://ftpmirror.gnu.org/automake | grep -E "automake.*tar.gz" | grep -vE "sig|asc" | awk -F \" '{print $4}' | sed 's/automake-//g' | sed 's/.tar.gz//g' | sort -n -k 2 -t "." | tail -n 1)
-echo && echo "check latest version of aotomake-${AUTOMAKE_VERSION}"
+echo && echo "check latest version of automake-${AUTOMAKE_VERSION}"
 LIBTOOL_VERSION=$(curl -fL http://ftpmirror.gnu.org/libtool | grep -E "libtool.*tar.gz" | grep -vE "sig|asc" | awk -F \" '{print $4}' | sed 's/libtool-//g' | sed 's/.tar.gz//g' | sort | tail -n 1)
 echo && echo "check latest version of libtool-${LIBTOOL_VERSION}"
+TEXINFO_VERSION=$(curl -fL http://ftpmirror.gnu.org/texinfo | grep -E "texinfo.*tar.gz" | grep -vE "sig|asc" | awk -F \" '{print $4}' | sed 's/texinfo-//g' | sed 's/.tar.gz//g' | sort | tail -n 1) 
+echo && echo "check latest version of texinfo-${TEXINFO_VERSION}"
 
 mkdir -p $BUILD_DIR && cd $BUILD_DIR
 
@@ -40,6 +42,13 @@ cd libtool-*
 make && sudo make install
 [ $? -eq 0 ] && LIBTOOL_ERROR=0 && cd .. && rm -rf libtool-*
 
+# Texinfo
+echo && echo "Installing the latest texinfo-${TEXINFO_VERSION}"
+curl -fL http://ftpmirror.gnu.org/texinfo/texinfo-${TEXINFO_VERSION}.tar.gz | tar xzf -
+cd texinfo-*
+./configure --prefix=/usr/local
+make && sudo make install
+[ $? -eq 0 ] && TEXINFO_ERROR=0 && cd .. && rm -rf texinfo-*
 
 if [ $AUTOCONF_ERROR -ne 0 ]; then
   echo "Autoconf error."
@@ -47,14 +56,16 @@ fi
 if [ $AUTOMAKE_ERROR -ne 0 ]; then
   echo "Automake error."
 fi
-
 if [ $LIBTOOL_ERROR -ne 0 ]; then
   echo "Libtool error."
+fi
+if [ $TEXINFO_ERROR -ne 0 ]; then
+  echo "Texinfo error."
 fi
 
 function add_path () {
     [[ ":${PATH}:" =~ .*:$1:.* ]] || PATH="${PATH:+${PATH}:}$1"
 }
 
-[ $AUTOCONF_ERROR -eq 0 ] && [ $AUTOMAKE_ERROR -eq 0 ] && [ $LIBTOOL_ERROR -eq 0 ] && echo "Installation complete." && cd .. && rmdir -fr $BUILD_DIR
+[ $AUTOCONF_ERROR -eq 0 ] && [ $AUTOMAKE_ERROR -eq 0 ] && [ $LIBTOOL_ERROR -eq 0 ] && [ $TEXINFO_ERROR -eq 0 ] && echo "Installation complete." && cd .. && rm -fr $BUILD_DIR
 
