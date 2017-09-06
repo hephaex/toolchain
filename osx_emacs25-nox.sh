9d7750dc367c630893ef88bc4bb7e98d8856c10f
Mario Cho <hephaex@gmail.com>
2017-09-07 02:17:28 +0900

    emacs-25.2-nox on OSX

--- osx_emacs25-nox.sh
+++ osx_emacs25-nox.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# Emacs build script for OSX 10.12.6 
+# Maintain by Mario Cho <hephaex@gmail.com>
+
 # wherever you'd like to build
 BUILD_DIR=obj
+
 # you'd like to build version of emacs
 #EMACS=25.2
 EMACS=$(curl -fL http://ftpmirror.gnu.org/emacs | grep -E "emacs.*tar.gz" | grep -vE "sig|asc" | awk -F \" '{print $4}' | sed 's/emacs-//g' |sed 's/lisp//g' | sed 's/.tar.gz//g' | sort | tail -n 1)
@@ -9,20 +13,21 @@ EMACS=$(curl -fL http://ftpmirror.gnu.org/emacs | grep -E "emacs.*tar.gz" | grep
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
-echo "build emacs version is ${EMACS}"
-
 # pull emacs source archive
-curl -LO http://ftpmirror.gnu.org/emacs/emacs-$EMACS.tar.xz
-# pull emacs patch file which 2byte code itterations on terminal env.
-#curl -LO https://gist.githubusercontent.com/takaxp/3314a153f6d02d82ef1833638d338ecf/raw/156aaa50dc028ebb731521abaf423e751fd080de/emacs-25.2-inline.patch
+echo "fetch emacs-${EMACS}"
+curl -fL http://ftpmirror.gnu.org/emacs/emacs-$EMACS.tar.gz | tar zxf -
+# pull emacs patch file which no title bar & unflicker on terminal env.
+curl -LO https:/github.com/hephaex/toolchain/raw/master/emacs-25.x.patch/emacs-25.2-OSX-no_title_bar.patch 
+curl -LO https://github.com/hephaex/toolchain/raw/master/emacs-25.x.patch/emacs-25.2-OSX-unflicker.patch
 
-# defile emacs source archive
-
-tar zxvf emacs-$EMACS.tar.xz
+# patch
 cd ./emacs-$EMACS
-#patch -p1 < ../emacs-$EMACS-inline.patch
-#sleep 5
-#./autogen.sh
+patch -p1 < ../emacs-25.2-OSX-no_title_bar.patch
+patch -p1 < ../emacs-25.2-OSX-unflicker.patch
+sleep 5
+
+# configure Makefile
+./autogen.sh
 ./configure --without-x --with-ns --with-modules
 
 # build
@@ -32,5 +37,6 @@ make bootstrap -j4
 # install 
 echo "Installing emacs-${EMACS}"
 make install -j4
-#cd ./nextstep
-#open .
+[ $? -eq 0 ] && EMACS_ERROR=0 && cp -R ./nextstep/Emacs.app /Applications/.
+[ $EMACS_ERROR -eq 0 ] && cd ../../ && rm -fr $BUILD_DIR && echo "emacs install complete !!!"
+alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs -nw"
