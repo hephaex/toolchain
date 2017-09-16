6a0652bd07796282f3dbca0086a216449d4274b5
Mario Cho <hephaex@gmail.com>
2017-09-16 11:49:46 +0900

    doxygen-1.8.7

--- doxygen-1.8.7/src/lang_cfg.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if (len(sys.argv) > 0):
-    if (sys.argv[1] == "ENONLY"):
-        print "#define ENGLISH_ONLY"
-    else:
-        for x in xrange(1, len(sys.argv)):
-            print "#define LANG_%s"%(sys.argv[x])
