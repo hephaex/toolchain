aa5ff40a39c8c0e296c3105342f9c68d51ef61d9
Mario Cho <hephaex@gmail.com>
2018-06-23 23:18:38 +0900

    Update python_env.sh

--- python_env.sh
+++ python_env.sh
@@ -26,6 +26,7 @@ cd $BUILD_DIR
 
 # pyenv install
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugin/virtualenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
