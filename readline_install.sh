ad11aca4152c869d1e5fbf1b538b6347b10a956f
Mario Cho <hephaex@gmail.com>
2018-08-03 16:27:28 +0900

    update readline-7.0

--- readline_install.sh
+++ readline_install.sh
@@ -6,20 +6,22 @@
 # or wherever you'd like to build
 BUILD_DIR=obj
 
-# Get the latest version of automake and libtool because they have no latest symbol link.
-echo && echo "check latest version of readline-${READLINE_VERSION}"
-READLINE_VERSION=$(curl -fL http://ftpmirror.gnu.org/texinfo | grep -E "texinfo.*tar.gz" | grep -vE "sig|asc" | awk -F \" '{print $4}' | sed 's/texinfo-//g' | sed 's/.tar.gz//g' | sort | tail -n 1) 
+echo && \
+    echo "check latest version of readline-${READLINE_VERSION}" && \
+    READLINE=7.0 && \
+    mkdir -p $BUILD_DIR && \
+    cd $BUILD_DIR
 
+echo "Installing readline-${READLINE}" && \
+    curl -fL http://git.savannah.gnu.org/cgit/readline.git/snapshot/readline-${READLINE}.tar.gz | tar xzf -
 
-mkdir -p $BUILD_DIR && cd $BUILD_DIR
+echo "build readline-${READLINE}" && \
+    cd readline-${READLINE} && \
+    ./configure --prefix=/usr/local && \
+    make && \
+    sudo make install
 
-
-echo "Installing automake-${AUTOMAKE_VERSION}..."
-curl -fL http://ftpmirror.gnu.org/automake/automake-${AUTOMAKE_VERSION}.tar.gz | tar xzf -
-cd automake-*
-./configure --prefix=/usr/local
-make && sudo make install
-[ $? -eq 0 ] && AUTOMAKE_ERROR=0 && cd .. && rm -rf automake-*
-
-[ $TEXINFO_ERROR -eq 0 ] && echo "Installation complete." && cd .. && rm -fr $BUILD_DIR
+echo "Installation complete." && \
+    cd .. && \
+    rm -fr $BUILD_DIR
 
