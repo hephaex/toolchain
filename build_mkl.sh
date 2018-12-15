bde82a79149448722adf7d1fec02b4054aad1527
Mario Cho <hephaex@gmail.com>
2018-12-15 22:28:10 +0900

    Create build_mkl.sh

---
+++ build_mkl.sh
@@ -0,0 +1,9 @@
+# If you don't have cmake
+sudo apt install cmake
+
+git clone https://github.com/01org/mkl-dnn.git
+cd mkl-dnn/scripts && ./prepare_mkl.sh && cd ..
+mkdir -p build && cd build && cmake .. && make
+sudo make install
+
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib' >> ~/.bashrc
