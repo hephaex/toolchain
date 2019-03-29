f0a13428752dd32505fe312974998cefebc54d5b
Mario Cho <hephaex@gmail.com>
2019-03-29 21:25:36 +0900

    Update install-emacs.sh

--- install-emacs.sh
+++ install-emacs.sh
@@ -2,6 +2,9 @@
 # Emacs build script for OSX 10.14.x
 # Maintain by Mario Cho <hephaex@gmail.com>
 
+# require dependencies 
+# brew update ; brew install autoconf
+
 git clone git://git.sv.gnu.org/emacs.git
 curl -LO https://gist.githubusercontent.com/takaxp/5294b6c52782d0be0b25342be62e4a77/raw/9c9325288ff03a50ee26e4e32c8ca57c0dd81ace/emacs-25.2-inline-googleime.patch
 
