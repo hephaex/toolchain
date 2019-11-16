03aa073cd38fb2e97355d32b5e0828292be8b1a6
Mario Cho <hephaex@gmail.com>
2019-11-17 00:19:42 +0800

    Update build_mkl.sh

--- build_mkl.sh
+++ build_mkl.sh
@@ -1,6 +1,6 @@
 # If you don't have cmake
 sudo apt install cmake
-
+export CMAKE_BUILD_TYPE=Release
 git clone https://github.com/01org/mkl-dnn.git
 cd mkl-dnn/scripts && ./prepare_mkl.sh && cd ..
 mkdir -p build && cd build && cmake .. && make
